8b2d574e8a8841de040287096208b0613411b19d
Alex <ajstrand8@gmail.com>
2022-01-26 00:17:51 -0500

    working on text editor tutorial. scrolliong isnt working

---
+++ src/editor.rs
@@ -0,0 +1,157 @@
+use crate::Document;
+use crate::Row;
+use crate::Terminal;
+use std::env;
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+#[derive(Default)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
+    offset: Position,
+    document: Document,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+    pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position::default(),
+            document,
+            offset: Position::default(),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position::default());
+        if self.should_quit {
+            Terminal::clear_screen();
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y)
+            });
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(pressed_key),
+            _ => (),
+        }
+        self.scroll();
+
+        Ok(())
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+
+        if y < offset.y {
+            offset.y = y
+        }
+        else if y >= offset.y.saturating_add(height){
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        }
+        else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y } = self.cursor_position;
+        let height = self.document.len();
+        let width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y > height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            _ => (),
+        }
+        self.cursor_position = Position { x, y }
+    }
+
+    pub fn draw_row(&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+
+        let end = self.offset.x + width;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+
+        for terminal_row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
+    panic!("{}", e);
+}
