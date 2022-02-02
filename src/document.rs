4ae2a68aa47566bea63ede4baf8ddf628a8e8ac6
Alex <ajstrand8@gmail.com>
2022-02-02 01:16:06 -0500

    wrote some more code from the tutorial

--- src/document.rs
+++ src/document.rs
@@ -1,3 +1,4 @@
+use crate::Position;
 use crate::Row;
 use std::fs;
 
@@ -8,7 +9,7 @@ pub struct Document {
 }
 
 impl Document {
-    pub fn open(filename: &str) -> Result<Self, std::io::Error>  {
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for value in contents.lines() {
@@ -19,7 +20,7 @@ impl Document {
             file_name: Some(filename.to_string()),
         })
     }
-    pub fn row(&self, index:usize) -> Option<&Row>{
+    pub fn row(&self, index: usize) -> Option<&Row> {
         self.rows.get(index)
     }
     pub fn is_empty(&self) -> bool {
@@ -27,5 +28,46 @@ impl Document {
     }
     pub fn len(&self) -> usize {
         self.rows.len()
+    }
+
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if c == '\n' {
+            self.insert_newline(at);
+            return;
+        }
+        if at.y == self.len() {
+            let mut row = Row::default();
+            row.insert(0, c);
+            self.rows.push(row);
+        } else if at.y < self.len() {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.insert(at.x, c);
+        }
+    }
+
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y >= len {
+            return;
+        }
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
+        }
+    }
+    fn insert_newline(&mut self, at: &Position) {
+        if at.y > self.len() {
+            return;
+        }
+        let new_row = Row::default();
+        if at.y == self.len() || at.y.saturating_add(1) == self.len() {
+            self.rows.push(new_row);
+        } else {
+            self.rows.insert(at.y + 1, new_row)
+        }
     }
 }
